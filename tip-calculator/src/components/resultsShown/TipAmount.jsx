c7d68e9435309e061efcb01996e5696b6cd03760
FlorinDobinciuc <florin.dobiniciuc@thinslices.com>
2022-09-15 11:44:20 +0300

    Add some minor changes

--- tip-calculator/src/components/resultsShown/TipAmount.jsx
+++ tip-calculator/src/components/resultsShown/TipAmount.jsx
@@ -1,9 +1,14 @@
 function TipAmount({ title }) {
   return (
-    <div className="tipAmount">
-      <h4>{title}</h4>
-      <span>/ person</span>
-    </div>
+    <>
+      <div className="tipAmount">
+        <h4>{title}</h4>
+        <span>/ person</span>
+      </div>
+      <div className="initial-amount">
+        <span>$ 0.00</span>
+      </div>
+    </>
   );
 }
 
